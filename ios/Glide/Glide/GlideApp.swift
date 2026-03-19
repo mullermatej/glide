59e9199c177720f833faaf25ccc606e0960e0f56
Matej Muller <mmuller@student.unipu.hr>
2026-03-19 16:56:00 +0100

    first commit

---
+++ ios/Glide/Glide/GlideApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlideApp.swift
+//  Glide
+//
+//  Created by Matej Muller on 19.03.2026..
+//
+
+import SwiftUI
+
+@main
+struct GlideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/Glide/GlideTests/GlideTests.swift
@@ -0,0 +1,17 @@
+//
+//  GlideTests.swift
+//  GlideTests
+//
+//  Created by Matej Muller on 19.03.2026..
+//
+
+import Testing
+@testable import Glide
+
+struct GlideTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
